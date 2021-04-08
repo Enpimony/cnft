c202ede302cd0750277c46331c83fd9bfad92b3f
Diego <dfabregas@gaa9.com>
2021-04-08 19:26:32 +0200

    Minor text fixes

--- src/app/constants/news.ts
+++ src/app/constants/news.ts
@@ -1,44 +1,45 @@
 const PROJECTS = {
-    fingerMonsters: {
-        name: 'Finger Monsters',
-        icon: 'assets/img/avatar/Finger monsters.jpg'
-    },
-    professorCardano: {
-        name: 'Professor Cardano',
-        icon: 'assets/img/avatar/Professor Cardano.jpg'
-    },
-    stigma: {
-        name: 'Stigma ',
-        icon: 'assets/img/avatar/StigmaNFT.jpg'
-    }
-}
+  fingerMonsters: {
+    name: 'Finger Monsters',
+    icon: 'assets/img/avatar/Finger monsters.jpg'
+  },
+  professorCardano: {
+    name: 'Professor Cardano',
+    icon: 'assets/img/avatar/Professor Cardano.jpg'
+  },
+  stigma: {
+    name: 'Stigma ',
+    icon: 'assets/img/avatar/StigmaNFT.jpg'
+  }
+};
 
 export const NEWS = [
-    {
-    date:'08 Apr 2021',
+  {
+    date: '08 Apr 2021',
     project: PROJECTS.stigma,
     title: 'The team revealed the first set of cards, preparing for the pre sale!',
     link: 'https://twitter.com/StigmaNFT/status/1380184812833619974',
-   
-},
-{
-    date:'08 Apr 2021',
+
+  },
+  {
+    date: '08 Apr 2021',
     project: PROJECTS.fingerMonsters,
     title: 'The first wave has been minted and distributed, accounting for 1000 pieces',
     link: 'https://twitter.com/CNFT_IO/status/1380061123924353024',
-   
-},
-{
-    date:'07 Apr 2021',
+
+  },
+  {
+    date: '07 Apr 2021',
     project: PROJECTS.professorCardano,
-    title: 'The second issue 117 Ts  has been minted and distributed',
+    title: 'The second issue 117 Ts has been minted and distributed',
     link: 'https://twitter.com/ProfesCardano/status/1379735214637846529',
-},
-{
-    date:'06 Apr 2021',
+  },
+  {
+    date: '06 Apr 2021',
     project: PROJECTS.professorCardano,
     title: 'The first issue 118 Og has been minted and distributed',
     link: 'https://twitter.com/ProfesCardano/status/1379499888136830977',
-}];
+  }
+];
 
 
